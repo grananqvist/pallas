6f2510e9350c8fec10bfdba038360e695dca0fe0
grananqvist <fille_granqvist@hotmail.com>
2024-11-07 02:18:49 +0100

    v13 n2n real

--- pallas-network/src/facades.rs
+++ pallas-network/src/facades.rs
@@ -182,7 +182,7 @@ impl PeerClient {
 
         let plexer = plexer.spawn();
 
-        let versions = handshake::n2n::VersionTable::v12(magic);
+        let versions = handshake::n2n::VersionTable::v13(magic);
 
         let handshake = handshake
             .handshake(versions)
--- pallas-network/src/miniprotocols/handshake/n2n.rs
+++ pallas-network/src/miniprotocols/handshake/n2n.rs
@@ -190,6 +190,22 @@ impl VersionTable {
 
         VersionTable { values }
     }
+
+    pub fn v13(network_magic: u64) -> VersionTable {
+        let values = vec![(
+            PROTOCOL_V13,
+            VersionData::new(
+                network_magic,
+                true,
+                Some(PEER_SHARING_DISABLED),
+                Some(false),
+            ),
+        )]
+        .into_iter()
+        .collect::<HashMap<u64, VersionData>>();
+
+        VersionTable { values }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
